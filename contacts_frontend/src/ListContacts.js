71e6a790eb40a2952f044ad632af4807467d8450
bryanh210 <tuongh@sas.upenn.edu>
2017-09-04 01:23:41 -0700

    Day 25: Component LifeCycle + React Router

--- contacts_frontend/src/ListContacts.js
+++ contacts_frontend/src/ListContacts.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 
@@ -71,11 +72,10 @@ clearQuery= () =>{
              value={query}
               onChange={(event) => this.updateQuery(event.target.value)}
             />
-            <a
-                href='#create'
-                onClick={this.props.onNavigate}
+            <Link
+                to='/create'
                 className='add-contact'
-             >Add Contact</a>
+             >Add Contact</Link>
           </div>
 
           {showingContacts.length !== contacts.length &&(
